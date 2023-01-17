75d1c813fc6a8741971d4dc3b282a8fc87191c6d
leticea <nascimento.leticeah@gmail.com>
2023-01-17 11:41:56 -0300

    inserindo icon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from 'react-icons/fi';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input type="text" placeholder="Digite seu cep..." />
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#fff" />
+        </button>
+      </div>
+
+      <main className="main">
+        <h2>CEP: 98765556</h2>
+
+        <span>Rua teste</span>
+        <span>Complemento: algum complemento</span>
+        <span>Alfeneiros</span>
+        <span>São Paulo - SP</span>
+
+      </main>
     </div>
   );
 }

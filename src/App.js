ebd5cf0f0c3088084bb844e60067b28bae25afbf
leticea <nascimento.leticeah@gmail.com>
2023-01-17 12:28:30 -0300

    criando readme

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import './styles.css';
 import api from './services/api';
 
 function App() {
-
   const [input, setInput] = useState('');
   const [cep, setCep] = useState({});
 
@@ -30,7 +29,6 @@ function App() {
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
-
       <div className="containerInput">
         <input
           type="text"
@@ -38,21 +36,20 @@ function App() {
           value={input}
           onChange={(e) => setInput(e.target.value)}
         />
-
         <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="#fff" />
         </button>
       </div>
 
-      <main className="main">
-        <h2>CEP: {cep.cep}</h2>
-
-        <span>{cep.logradouro}</span>
-        <span>Complemento: {cep.complemento}</span>
-        <span>{cep.bairro}</span>
-        <span>{cep.localidade} - {cep.uf}</span>
-
-      </main>
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+          <span>{cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+        </main>
+      )}
     </div>
   );
 }
